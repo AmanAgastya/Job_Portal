bc45823611a640061d836a820eee761363c038ef
Aman Agastya <amanagastya0709@gmail.com>
2026-05-01 21:31:02 +0530

    remove api link calling from render

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api" || 'https://job-portal-backend-o3d5.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://job-portal-backend-o3d5.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 
