975441e09d3adf3625817eed7c15083ef0627065
Aman Agastya <amanagastya0709@gmail.com>
2026-05-02 02:03:13 +0530

    Policy

--- frontend/src/pages/PrivacyPage.js
+++ frontend/src/pages/PrivacyPage.js
@@ -30,9 +30,9 @@ export default function PrivacyPage() {
           <aside className="policy-toc" aria-labelledby="privacy-contents-title">
             <div id="privacy-contents-title" className="policy-toc-title">Contents</div>
             {SECTIONS.map(s => (
-              // <a key={s.id} href={`#${s.id}`} className="policy-toc-link">
+              <a key={s.id} className="policy-toc-link">
                 {s.title}
-             // </a>
+              </a>
             ))}
           </aside>
 
